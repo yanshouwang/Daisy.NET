4304429acb2c2785642af90a8693075c0a844ee2
iAMD <yanshouwang@outlook.com>
2020-07-23 11:33:13 +0800

    整理 20200722 内容

---
+++ 20200722/HelloWorld.WinForm/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace HelloWorld.WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbShowHello = new System.Windows.Forms.TextBox();
+            this.btnSayHello = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbShowHello
+            // 
+            this.tbShowHello.Location = new System.Drawing.Point(12, 12);
+            this.tbShowHello.Name = "tbShowHello";
+            this.tbShowHello.Size = new System.Drawing.Size(776, 23);
+            this.tbShowHello.TabIndex = 0;
+            // 
+            // btnSayHello
+            // 
+            this.btnSayHello.Location = new System.Drawing.Point(12, 42);
+            this.btnSayHello.Name = "btnSayHello";
+            this.btnSayHello.Size = new System.Drawing.Size(776, 23);
+            this.btnSayHello.TabIndex = 1;
+            this.btnSayHello.Text = "Click";
+            this.btnSayHello.UseVisualStyleBackColor = true;
+            this.btnSayHello.Click += new System.EventHandler(this.btnSayHello_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSayHello);
+            this.Controls.Add(this.tbShowHello);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbShowHello;
+        private System.Windows.Forms.Button btnSayHello;
+    }
+}
+
---
+++ 20200722/README.md
@@ -0,0 +1,62 @@
+### C# 语言简介
+
+- 什么是程序
+- 为什么需要程序
+- 怎样编写程序和程序语言的选择
+    - 编辑 -> 编译 -> 调试 -> 发布
+- 编程的学习路径
+    - 纵向: 语言 -> 类库 -> 框架
+    - 横向: 命令行程序, 桌面程序, 设备 (平板/手机) 程序, Web (网站/服务) 程序, 游戏...
+
+### 开发环境
+
+- Visual Studio, Rider...
+
+### 学习资料
+
+- [Microsoft Docs](https://docs.microsoft.com/)
+- [C# 语言规范](https://ecma-international.org/publications/standards/Ecma-334.htm)
+- 《C# X.0 IN A NUTSHELL》(果壳中的 C#)
+- 《深入浅出 WPF》
+
+### 编程学习的捷径
+
+- 编程不是 "学" 出来的, 而是 "练" 出来的
+- 在反复应用中积累, 忽然有一天就会 "顿悟"
+- 学习原则
+    - 从感观到原理
+    - 从使用别人的到创建自己的
+    - 必须亲自动手
+    - 必须学以致用, 紧跟实际工作
+    - 追求实用, 不搞 "学院派"
+
+### 第一个程序: Hello World!
+
+- Solution 和 Project
+    - Sulotion 是针对客户需求的总的解决方案。举例: 汽车经销商需要一套销售软件
+    - Project 解决具体的某个问题
+- Project 模板
+- 分别编写 Console, WPF, Windows Forms 的 Hello World 程序
+- 初学编程时很重要的两点
+    - 不要怕见到自己看不懂的东西
+    - 要能跟着操作, 一遍一遍地练习, 为的是熟悉手里的工具, 培养感觉
+
+### 见识 C# 编写的各类应用程序
+
+- Console
+- WPF (Windows Presentation Foundation) *
+- Windows Forms
+- ASP.NET Web Forms
+- ASP.NET MVC *
+- WCF (Windows Communication Foundation) *
+- Windwos Store Application *
+- Windows Phone Application *
+- Cloud(Azure) *
+- WF (Workflow Foundation)
+
+### 本节作业
+
+- 熟悉 Visual Studio 的界面和使用
+- 尝试浏览 Microsoft Docs 相关内容
+- 编写 Console, WPF, Windows Forms 的 Hello World 程序
+- 有兴趣可以对照视频编写其他版本的 Hello World 程序
