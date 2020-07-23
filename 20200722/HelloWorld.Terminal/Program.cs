d519bb2f22c71f3c11b88643f21c456e7d2242c7
iDotNET <yanshouwang@outlook.com>
2020-07-23 11:38:04 +0800

    20200722

---
+++ 20200722/HelloWorld.Terminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ 20200722/HelloWorld.TerminalCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tools;
+
+namespace HelloWorld.TerminalCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Calculator calculator = new Calculator();
+            double sum = calculator.Sub(5.0, 1.2);
+            Console.WriteLine(sum);
+        }
+    }
+}
