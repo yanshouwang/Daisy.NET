c7a69f642135142b5ce9a39179b342c41664ed09
iAMD <yanshouwang@outlook.com>
2020-07-23 22:33:42 +0800

    修复问题

---
+++ 20200723/Tools/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tools
+{
+    public class Calculator
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Sub(double x, double y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
