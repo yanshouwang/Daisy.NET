721ae480298059d8ff48be74d9e53e3bce58406b
iAMD <yanshouwang@outlook.com>
2020-07-28 15:44:07 +0800

    20200728

---
+++ 20200722/HelloWorld.TerminalTests/CalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloWorld.Terminal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Terminal.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void GetCircleAeraTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetCylinderVolumeTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetConeVolumeTest()
+        {
+
+        }
+    }
+}
